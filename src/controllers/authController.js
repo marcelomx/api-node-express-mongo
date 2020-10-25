5e462690ed3c85fcc857336b4c2dce33f62ef987
Marcelo Rodrigues Gonzaga <marcelo.gonzaga@presidencia.gov.br>
2020-10-25 15:38:37 -0300

    Add authentication with JWT

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const authConfig = require("../config/auth");
 
 const User = require("../models/user");
 
 const router = express.Router();
+
+function generateToken(params = {}) {
+  return jwt.sign(params, authConfig.secret, {
+    expiresIn: 86400,
+  });
+}
 
 router.post("/register", async (req, res) => {
   const { email } = req.body;
@@ -12,10 +21,33 @@ router.post("/register", async (req, res) => {
     }
     const user = await User.create(req.body);
     user.password = undefined;
-    return res.send({ user });
+
+    const token = generateToken({ id: user.id });
+
+    return res.send({ user, token });
   } catch (e) {
     return res.status(400).send({ error: "Registration failed" });
   }
+});
+
+router.post("/authenticate", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return res.status(400).send({ error: "User not found" });
+  }
+
+  if (!(await bcrypt.compare(password, user.password))) {
+    return res.status(400).send({ error: "Password is invalid" });
+  }
+
+  user.password = undefined; // not return password!
+
+  const token = generateToken({ id: user.id });
+
+  return res.send({ user, token });
 });
 
 module.exports = (app) => app.use("/auth", router);
