6b13ede46642af76ec982b45ee15e974597f17fc
Marcelo Rodrigues Gonzaga <marcelo.gonzaga@presidencia.gov.br>
2020-10-25 15:20:49 -0300

    Add email/password validation

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,9 +5,13 @@ const User = require("../models/user");
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
+  const { email } = req.body;
   try {
-    console.log("Registering user");
+    if (await User.findOne({ email })) {
+      return res.status(400).send({ error: "User already exists" });
+    }
     const user = await User.create(req.body);
+    user.password = undefined;
     return res.send({ user });
   } catch (e) {
     return res.status(400).send({ error: "Registration failed" });
